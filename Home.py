e89c9c5847d38151693d49f3279e675eddbcf3db
heuristic_machina <machine_learner@yahoo.com>
2025-12-04 10:35:17 -0700

    minor edit

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title='Coronary Artery Disease Dataset Explorer', page_i
 
 
 st.title('Coronary Artery Disease Dataset Explorer')
-st.subheader('Welcome the the Heart dataset explorer app')
+st.subheader('Welcome to the Heart dataset explorer app')
 st.write("""
     This app provides an interactive platform to explore the famous Heart dataset.
          You can visualize the distribution of data in Data-Overview and Dictionary pages, explore relationships between features on Exploratory-Analysis page, and even make your own confusion matrix model on Make-Your-Own-Matrix page!  When your done playing around check out the Prediction-Insights for results performed on this dataset using Random Forest Classifier.
