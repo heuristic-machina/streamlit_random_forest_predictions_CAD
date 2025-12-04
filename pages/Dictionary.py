7d049591dcf202c0c981d14df398451a9aa036ea
heuristic_machina <machine_learner@yahoo.com>
2025-12-03 22:20:21 -0700

    ux/ui update

--- pages/Dictionary.py
+++ pages/Dictionary.py
@@ -3,17 +3,46 @@ import streamlit as st
 #container
 st.title('Heart Data Dictionary 📖')
 container = st.container(border=True)
-container.write('age: Age of the patient')
-container.write('sex: Gender of the patient (0: female, 1: male)')
-container.write('cp: Chest pain type (0: typical angina, 1: atypical angina, 2: non-anginal pain, 3: asymptomatic)')
-container.write('trestbps: Resting blood pressure (in mm/Hg)')
-container.write('chol: Serum cholesterol (in mg/dl)')
-container.write('trestbps: Resting blood pressure (in mm/Hg)')
-container.write('fbs: Fasting blood sugar (> 120 mg/dl) 0: true, 1: false')
-container.write('restecg: Resting electrocardiographic results (0: normal, 1: ST-T wave abnormality, 2: left ventricular hypertrophy sign)')
-container.write('thalach: Maximum heart rate achieved')
-container.write('exang: Exercise-induced angina (0: no, 1: yes)')
-container.write('oldpeak: ST depression induced by exercise relative to rest')
-container.write('slope: The slope of the peak exercise ST segment')
-container.write('ca: Number of major vessels (0-3) colored by fluoroscopy')
-container.write('thal: A blood disorder called Thalassemia (0: error, 1: fixed defect, 2: normal, 3: reversible defect)')
+container.write('1. age: Age of the patient')
+container.markdown("""
+2. sex: Gender of the patient:
+ - 0: Female
+ - 1: Male   
+""")
+container.markdown("""
+3. cp: Chest pain type:
+ - 0: Typical angina
+ - 1: Atypical angina
+ - 2: Non-anginal pain
+ - 3: Asymptomatic       
+""")
+container.write('4. trestbps: Resting blood pressure (in mm/Hg)')
+container.write('5. chol: Serum cholesterol (in mg/dl)')
+container.write('6. trestbps: Resting blood pressure (in mm/Hg)')
+container.markdown("""
+7. fbs: Fasting blood sugar (> 120 mg/dl) 0:
+ - 0: True
+ - 1: False   
+""")
+container.markdown("""
+8. restecg: Resting electrocardiographic results:
+ - 0: Normal
+ - 1: ST-T wave abnormality
+ - 2: Left ventricular hypertrophy sign    
+""")
+container.write('9. thalach: Maximum heart rate achieved')
+container.markdown("""
+10. exang: Exercise-induced angina:
+ - 0: No
+ - 1: Yes   
+""")
+container.write('11. oldpeak: ST depression induced by exercise relative to rest')
+container.write('12. slope: The slope of the peak exercise ST segment')
+container.write('13. ca: Number of major vessels (0-3) colored by fluoroscopy')
+container.markdown("""
+14. cp: thal: A blood disorder called Thalassemia:
+ - 0: Error
+ - 1: Fixed defect
+ - 2: Normal
+ - 3: Reversible defect       
+""")
--- pages/Insights.py
+++ pages/Insights.py
@@ -2,15 +2,50 @@ import streamlit as st
 
 #tabs
 st.title('Insights and Conclusion')
-tab1, tab2, tab3, tab4 = st.tabs(['Machine Learning', 'Precision', 'Recall', 'Top Factors'])
+tab1, tab2, tab3, tab4 = st.tabs(['Top Factors', 'Machine Learning', 'Precision', 'Recall'])
 
 with tab1:
-    st.write('🌳 Random Forest is very reliable and accurate with a score of 99.5 % percent accuracy in predicting patients with heart disease given the 14 tested features')
+    st.header('💓 Top Factors')
+    st.markdown("""
+The most influential important factors in predicting coronary artery disease are:
+ - Chest pain
+ - Number of vessels colored by fluroscopy
+ - Max heart rate
+ - Exercise-induced vs resting ST depression
+ - Thalassemia blood disorder        
+""")
+    st.image('imgs/factors_hd.png', caption="Important Factors")
 with tab2:
-    st.write('🧪 The precision scored perfectly at 100 % and saves the patient from unnecessary stress tests and unneeded costs')
+    st.header('🌳 Random Forest Classifier')
+    st.write('Using replacement random selection on the dataset\'s 14 predictor features yeilded 99.5 % accuracy and is very reliable in predicting heart disease in patients')
+    st.divider()
+    st.header('Reduce Noise')
+    container = st.container(border=True)
+    container.write('Random trees reduce the variance and avoids correlation of the trees giving the classification model a more reliable majority vote')
+    st.write('The prediction of x is the average of B predictions:')
+    st.latex(r'''
+    y = f(x) =
+    \frac{1}{B}\ \sum_{b=1}^{B} f_{b}(x)
+    ''')
 with tab3:
-    st.write('🔬 The recall of 97 % detected virtually all patients with heart disease which is insightful as being clinically safe')
+    st.header('🧪 Precision')
+    st.write('The model predicted the true positives 100% of the time and is impactful for customer care as unnessessary testing is avoided')
+    st.image('imgs/matrix_hd.png', width=500, caption="Confusion Matrix True Positives: 100")
+    st.divider()
+    st.header('Precision')
+    container = st.container(border=True)
+    container.write('Precision is the ratio of true positives to the total of true positives and false positives')
+    st.latex(r'''
+        Precision = \left(\frac{True Positives}{True Positives + False Positives}\right)
+    ''')    
 with tab4:
-    st.write('💓 The most influential/important factors are: chest pain, number of vessels colored by fluroscopy, max heart rate, exercise-induced vs resting ST depression, and the Thalassemia blood disorder')
-    
-st.image('imgs/insights.jpg', caption="Insights")
+    st.header('🔬 Test Sensitivity')
+    st.write('The testing sensitivity is clinically safe detecting 97% of all patients with heart disease')
+    st.image('imgs/matrix_hd.png', width=500, caption="Confusion Matrix")
+    st.divider()
+    st.header('Recall')
+    container = st.container(border=True)
+    container.write('Recall measures the proportion of actual positive cases that were correctly identified by a model or test')
+    st.latex(r'''
+        Recall = \left(\frac{True Positives}{True Positives + False Negatives}\right)
+    ''')    
